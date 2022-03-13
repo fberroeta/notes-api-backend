241a4ed30544e3de577189185993374d4c566232
fberroeta <fsberroeta@gmail.com>
2022-03-13 07:25:19 -0300

    add heroku needed config

--- index.js
+++ index.js
@@ -79,7 +79,9 @@ app.use((request,response)=>{
   response.status(404).json({ error: 'Not found'})
 })
 
-const PORT = 3001;
+// const PORT = 3001;
+const PORT = process.env.PORT || 3001;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
