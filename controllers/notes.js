3e73c5ed92664b1f7be5bfb3b59a68e15ed7b5d2
fberroeta <fsberroeta@gmail.com>
2022-03-20 11:18:17 -0300

    notes controller fixed, populate user

--- controllers/notes.js
+++ controllers/notes.js
@@ -1,116 +1,112 @@
 const notesRouter = require('express').Router();
 const Note = require('../models/Note');
 
-notesRouter.post('/', async (request,response)=>{
-  const {body} = request; 
-  const{content,date,important} = body;
+notesRouter.post('/', async (request, response) => {
+  const { body } = request;
+  const { content, date, important } = body;
 
   const notes = new Note({
     content,
     date,
-    important
-    
+    important,
   });
 
   const savedNotes = await notes.save();
 
   response.json(savedNotes);
-
 });
 
-// notesRouter.get('/', async (request, response) => {
-//   //arreglo para el timeout en la promesa
-//   // await (new Promise(resolve => setTimeout(resolve, 3000))); 
-//   // Note.find({}).then(notes=>{    
-//   const notes = await Note.find({});  
-//   response.json(notes);
-// });
+notesRouter.get('/', async (request, response) => {
+  //arreglo para el timeout en la promesa
+  // await (new Promise(resolve => setTimeout(resolve, 3000)));
+  // Note.find({}).then(notes=>{
+  const notes = await Note.find({}).populate('user');
+  response.json(notes);
+});
 
 // });
-
 
 // //para que los errores que son frecuentes no salga el 404 ocupar midelware
-// notesRouter.get('/:id', (request, response,next) => {
-//   const {id} = request.params;
-//   Note.findById(id).then(note=>{
-//     note ? response.json(note) : response.status(404).end();
-//   }).catch(err => {
-//     // console.error(err);
-//     // response.status(400).end();
-//     next(err);
-//   });
-// });
+notesRouter.get('/:id', (request, response, next) => {
+  const { id } = request.params;
+  Note.findById(id)
+    .then((note) => {
+      note ? response.json(note) : response.status(404).end();
+    })
+    .catch((err) => {
+      // console.error(err);
+      // response.status(400).end();
+      next(err);
+    });
+});
 
-// notesRouter.put('/:id', (request, response,next) => {
+notesRouter.put('/:id', (request, response, next) => {
+  const { id } = request.params;
+  const note = request.body;
 
-//   const {id} = request.params;
-//   const note = request.body;
- 
-//   const newNoteInfo = {
-//     content: note.content,
-//     important:note.important
-//   };
-//   Note.findByIdAndUpdate(id, newNoteInfo).
-//     then(result=>{
-//       response.json(result);
-//     })
-//     .catch(next);
-  
-// });
+  const newNoteInfo = {
+    content: note.content,
+    important: note.important,
+  };
+  Note.findByIdAndUpdate(id, newNoteInfo)
+    .then((result) => {
+      response.json(result);
+    })
+    .catch(next);
+});
 
 // //sin async
 // // app.delete('/api/notes/:id', (request, response,next) => {
-// //   const {id} = request.params;  
-  
+// //   const {id} = request.params;
+
 // //   Note.findByIdAndDelete(id)
 // //     .then(()=>response.status(204).end())
 // //     .catch(next);
 
 // // });
 // //con async
-// notesRouter.delete('/:id', async (request, response,next) => {
-//   const {id} = request.params;  
-  
-//   await Note.findByIdAndDelete(id)
-//     .then(()=>response.status(204).end());
-    
+notesRouter.delete('/:id', async (request, response, next) => {
+  const { id } = request.params;
 
-// });
+  await Note.findByIdAndDelete(id).then(() => response.status(204).end());
 
-// //sin async
-// // app.post('/api/notes', (request, response,next) => {
-// //   const note = request.body;
+  // });
 
-// //   if (!note.content)return(response.status(400).json({ error: 'required content is missing' }));
-// //   const newNote = new Note({ 
-// //     content: note.content,
-// //     important: note.important || false, 
-// //     date: new Date(),
-// //   }); 
+  // //sin async
+  // // app.post('/api/notes', (request, response,next) => {
+  // //   const note = request.body;
 
-// //   newNote.save().then(savedNote =>{
-// //     response.json(savedNote);
-// //   }).catch(err=>next(err));
-  
-// // });
+  // //   if (!note.content)return(response.status(400).json({ error: 'required content is missing' }));
+  // //   const newNote = new Note({
+  // //     content: note.content,
+  // //     important: note.important || false,
+  // //     date: new Date(),
+  // //   });
 
-// //con async
-// notesRouter.post('/', async (request, response,next) => {
-//   const note = request.body;
+  // //   newNote.save().then(savedNote =>{
+  // //     response.json(savedNote);
+  // //   }).catch(err=>next(err));
 
-//   if (!note.content)return(response.status(400).json({ error: 'required content is missing' }));
-//   const newNote = new Note({ 
-//     content: note.content,
-//     important: note.important || false, 
-//     date: new Date(),
-//   }); 
+  // // });
 
-//   try {
-//     const savedNote = await newNote.save();
-//     response.json(savedNote);    
-//   } catch (error) {
-//     next(error);
-//   }
-// });
+  // //con async
+  // notesRouter.post('/', async (request, response,next) => {
+  const note = request.body;
+
+  if (!note.content)
+    return response.status(400).json({ error: 'required content is missing' });
+  const newNote = new Note({
+    content: note.content,
+    important: note.important || false,
+    date: new Date(),
+  });
+
+  try {
+    const savedNote = await newNote.save();
+    response.json(savedNote);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = notesRouter;
--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,12 @@ const usersRouter = require('express').Router();
 const User = require('../models/User');
 
 usersRouter.get('/', async (request, response) => {
-  const users = await User.find({}).populate('notes');
+  const users = await User.find({}).populate('notes',
+    {
+      content:1,
+      date:1
+    }
+  );
   response.json(users); 
 });
 
